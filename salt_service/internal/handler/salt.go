e4aa4560791f45e746510cb4e1c429313d10ff4c
rshezarr <rshezarr@gmail.com>
2023-02-14 23:23:25 +0600

    add: generate salt handler

---
+++ salt_service/internal/handler/salt.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"salt_srv/internal"
+)
+
+func (h *Handler) generateSalt(w http.ResponseWriter, r *http.Request) {
+	saltResp := internal.Salt{
+		GeneratedSalt: h.service.Salt.GenerateSalt(),
+	}
+
+	if err := json.NewEncoder(w).Encode(saltResp); err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
