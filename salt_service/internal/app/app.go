239d42ae211521cdc09bc724adcf8d7e52817da1
rshezarr <rshezarr@gmail.com>
2023-02-14 23:27:19 +0600

    add: app constructor

--- salt_service/internal/app/app.go
+++ salt_service/internal/app/app.go
@@ -3,9 +3,21 @@ package app
 import (
 	"salt_srv/internal"
 	"salt_srv/internal/handler"
+	"salt_srv/internal/usecase"
 )
 
 type App struct {
 	httpServer  *internal.Server
 	httpHandler *handler.Handler
+}
+
+func New() (*App, error) {
+	usecase := usecase.NewUsecase()
+
+	httpHandler := handler.NewHandler(usecase)
+
+	return &App{
+		httpHandler: httpHandler,
+		httpServer:  internal.NewServer(httpHandler.InitRoutes()),
+	}, nil
 }
