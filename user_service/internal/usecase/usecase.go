3aa79f1dd67a4ea7a92b8743aa574a6e2468bb65
rshezarr <rshezarr@gmail.com>
2023-02-14 23:50:57 +0600

    link: user usecase methods

--- user_service/internal/usecase/usecase.go
+++ user_service/internal/usecase/usecase.go
@@ -1,1 +1,13 @@
 package usecase
+
+import "user_svc/internal/repository"
+
+type Usecase struct {
+	User User
+}
+
+func NewUsecase(repo *repository.Repository) *Usecase {
+	return &Usecase{
+		User: NewUser(repo),
+	}
+}
