ed598cb0668a28ec76af196e26f2119661d02a37
rshezarr <rshezarr@gmail.com>
2023-02-14 23:18:50 +0600

    implement: salt methods

--- salt_service/internal/usecase/generate_salt.go
+++ salt_service/internal/usecase/generate_salt.go
@@ -1,5 +1,7 @@
 package usecase
 
+import "math/rand"
+
 type Salt interface {
 	GenerateSalt() string
 }
@@ -8,4 +10,15 @@ type SaltUsecase struct{}
 
 func NewSaltUsecase() *SaltUsecase {
 	return &SaltUsecase{}
+}
+
+func (s *SaltUsecase) GenerateSalt() string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, 12)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(b)
 }
