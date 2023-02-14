c31aa94c460221663b45970820e43e9b758cd1e7
rshezarr <rshezarr@gmail.com>
2023-02-14 23:52:03 +0600

    add: handler entity

--- user_service/internal/handler/handler.go
+++ user_service/internal/handler/handler.go
@@ -1,1 +1,19 @@
 package handler
+
+import (
+	"user_svc/internal/usecase"
+
+	"github.com/go-chi/chi"
+)
+
+type Handler struct {
+	router  *chi.Mux
+	service *usecase.Usecase
+}
+
+func NewHandler(usecase *usecase.Usecase) *Handler {
+	return &Handler{
+		router:  chi.NewRouter(),
+		service: usecase,
+	}
+}
