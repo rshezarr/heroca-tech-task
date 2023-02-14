9d77361b31b3f7482bc31ce44ef7adb07a77b8ff
rshezarr <rshezarr@gmail.com>
2023-02-14 23:25:00 +0600

    set: server configs

--- salt_service/internal/server.go
+++ salt_service/internal/server.go
@@ -1,1 +1,24 @@
 package internal
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	server *http.Server
+}
+
+func NewServer(router *chi.Mux) *Server {
+	return &Server{
+		server: &http.Server{
+			Addr:           ":9090",
+			Handler:        router,
+			WriteTimeout:   10 * time.Second,
+			ReadTimeout:    10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		},
+	}
+}
