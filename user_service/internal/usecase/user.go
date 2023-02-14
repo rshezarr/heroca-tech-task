72928eecf0ecef4fea6783519083367717390d47
rshezarr <rshezarr@gmail.com>
2023-02-14 23:49:37 +0600

    add: user usecase entity

--- user_service/internal/usecase/user.go
+++ user_service/internal/usecase/user.go
@@ -3,9 +3,20 @@ package usecase
 import (
 	"context"
 	"user_svc/internal"
+	"user_svc/internal/repository"
 )
 
 type User interface {
 	Create(ctx context.Context, user internal.User, salt string) error
 	Get(ctx context.Context, email string) (internal.User, error)
+}
+
+type UserUsecase struct {
+	repo *repository.Repository
+}
+
+func NewUser(repo *repository.Repository) *UserUsecase {
+	return &UserUsecase{
+		repo: repo,
+	}
 }
