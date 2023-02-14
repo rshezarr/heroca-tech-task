e07a88aee3a92839e1adb5638d9fb1784479b216
rshezarr <rshezarr@gmail.com>
2023-02-14 23:27:34 +0600

    add: start app method

--- salt_service/internal/app/app.go
+++ salt_service/internal/app/app.go
@@ -1,9 +1,14 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
 	"salt_srv/internal"
 	"salt_srv/internal/handler"
 	"salt_srv/internal/usecase"
+	"syscall"
 )
 
 type App struct {
@@ -20,4 +25,25 @@ func New() (*App, error) {
 		httpHandler: httpHandler,
 		httpServer:  internal.NewServer(httpHandler.InitRoutes()),
 	}, nil
+}
+
+func (a *App) Start() error {
+	go func() {
+		if err := a.httpServer.StartServer(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	log.Println("generate-salt server started on :9090")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigChan
+	fmt.Println()
+	log.Println("Received terminate, graceful shutdown", sig)
+
+	if err := a.httpServer.Shutdown(); err != nil {
+		return err
+	}
+
+	return nil
 }
