279104cec88f22db55e48eff3e4a2f61f7744f80
rshezarr <rshezarr@gmail.com>
2023-02-14 23:45:34 +0600

    install: mongo dependency

--- user_service/internal/repository/user.go
+++ user_service/internal/repository/user.go
@@ -3,9 +3,21 @@ package repository
 import (
 	"context"
 	"user_svc/internal"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type User interface {
 	Create(ctx context.Context, user internal.User) error
 	Get(ctx context.Context, email string) (internal.User, error)
+}
+
+type UserRepo struct {
+	db *mongo.Collection
+}
+
+func NewUser(database *mongo.Database, collection string) *UserRepo {
+	return &UserRepo{
+		db: database.Collection(collection),
+	}
 }
