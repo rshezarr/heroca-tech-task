10b2c0457137ab0822e6bf02d54f617744dc527b
rshezarr <rshezarr@gmail.com>
2023-02-15 00:00:39 +0600

    add: mongo db new client

--- user_service/pkg/database/mongodb/mongodb.go
+++ user_service/pkg/database/mongodb/mongodb.go
@@ -1,1 +1,24 @@
 package mongodb
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewClient(ctx context.Context) (*mongo.Database, error) {
+	cliOptions := options.Client().ApplyURI("mongodb://mongo_db:27017")
+
+	client, err := mongo.Connect(ctx, cliOptions)
+	if err != nil {
+		return nil, fmt.Errorf("connect: %w", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("ping: %w", err)
+	}
+
+	return client.Database("user-service"), nil
+}
