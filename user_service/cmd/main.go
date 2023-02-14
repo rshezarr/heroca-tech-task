1eac0656a5b5a84cbe36aeaef2b6b8abc9e02eab
rshezarr <rshezarr@gmail.com>
2023-02-15 00:04:16 +0600

    add: main: run and init app

--- user_service/cmd/main.go
+++ user_service/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"user_svc/internal/app"
+)
 
+func main() {
+	app, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.RunApp(); err != nil {
+		log.Fatal(err)
+	}
 }
