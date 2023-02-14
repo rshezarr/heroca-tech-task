a643563983073b0738c124d08f496ebf96763f24
rshezarr <rshezarr@gmail.com>
2023-02-14 23:48:02 +0600

    link: repository methods and interface

--- user_service/internal/repository/repository.go
+++ user_service/internal/repository/repository.go
@@ -1,1 +1,13 @@
 package repository
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type Repository struct {
+	User User
+}
+
+func NewRepository(db *mongo.Database) *Repository {
+	return &Repository{
+		User: NewUser(db, "users"),
+	}
+}
