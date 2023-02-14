d2c628fe5056d52493e1055124a6089c8db63242
rshezarr <rshezarr@gmail.com>
2023-02-14 23:28:17 +0600

    add: main: init and run app

--- salt_service/cmd/main.go
+++ salt_service/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"salt_srv/internal/app"
+)
 
+func main() {
+	a, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := a.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
