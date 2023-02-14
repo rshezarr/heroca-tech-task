0d26d49a571f187b9f5836ec10130b18e40dbfd6
rshezarr <rshezarr@gmail.com>
2023-02-14 23:20:02 +0600

    update: salt handler: init handler entity

--- salt_service/go.mod
+++ salt_service/go.mod
@@ -1,3 +1,5 @@
 module salt_srv
 
 go 1.20
+
+require github.com/go-chi/chi v1.5.4
--- salt_service/internal/handler/handler.go
+++ salt_service/internal/handler/handler.go
@@ -1,1 +1,19 @@
 package handler
+
+import (
+	"salt_srv/internal/usecase"
+
+	"github.com/go-chi/chi"
+)
+
+type Handler struct {
+	router  *chi.Mux
+	service *usecase.Usecase
+}
+
+func NewHandler(service *usecase.Usecase) *Handler {
+	return &Handler{
+		router:  chi.NewRouter(),
+		service: service,
+	}
+}
