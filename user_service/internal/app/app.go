300fd98f202ee33b36b126f8005a90bce4df8db1
rshezarr <rshezarr@gmail.com>
2023-02-14 23:58:34 +0600

    add: new app constuctor

--- user_service/internal/app/app.go
+++ user_service/internal/app/app.go
@@ -1,10 +1,32 @@
 package app
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type App struct {
 	db *mongo.Database
 
 	httpServer  *Server
 	httpHandler *handler.Handler
+}
+
+func New() (*App, error) {
+	db, err := mongodb.NewClient(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	repo := repository.NewRepository(db)
+	usecase := usecase.NewUsecase(repo)
+
+	httpHandler := httpHandler.NewHandler(usecase)
+
+	return &App{
+		db:          db,
+		httpServer:  server.NewServer(httpHandler.InitRoutes()),
+		httpHandler: httpHandler,
+	}, nil
 }
