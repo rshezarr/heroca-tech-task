ce3f77e69d9e002e20229ce5572d70dbff34850e
rshezarr <rshezarr@gmail.com>
2023-02-14 23:43:50 +0600

    add: user interface

--- user_service/internal/repository/user.go
+++ user_service/internal/repository/user.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+	"context"
+	"user_svc/internal"
+)
+
+type User interface {
+	Create(ctx context.Context, user internal.User) error
+	Get(ctx context.Context, email string) (internal.User, error)
+}
